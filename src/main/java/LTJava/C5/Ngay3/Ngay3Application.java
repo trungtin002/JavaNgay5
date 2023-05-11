f1a7506fd7b388cd249073406e409736a103473b
luung <luung@DESKTOP-U1L50OA>
2023-05-11 13:06:08 +0700

    First Commit

---
+++ src/main/java/LTJava/C5/Ngay3/Ngay3Application.java
@@ -0,0 +1,13 @@
+package LTJava.C5.Ngay3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ngay3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ngay3Application.class, args);
+	}
+
+}
---
+++ src/main/java/LTJava/C5/Ngay3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package LTJava.C5.Ngay3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ngay3Application.class);
+	}
+
+}
